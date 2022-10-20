6878bbe1f3c6f3a37da4982330dad557c926252e
Ana <a.canelhas@campus.fct.unl.pt>
2022-10-20 15:53:20 +0100

    added comments

--- src/scc/utils/TestJackson.java
+++ src/scc/utils/TestJackson.java
@@ -10,6 +10,7 @@ public class TestJackson {
         public int id;
         public String name;
 
+        //you always need an empty constructor for the mapper to be able to deserialize the object
         public MyUser() { }
         public MyUser(int id, String name) {
             this.id = id;
@@ -19,6 +20,7 @@ public class TestJackson {
 
     public static void main(String[] args) {
 
+        //create a custom object
         MyUser user1 = new MyUser(1, "John");
         MyUser user2 = new MyUser(2, "Jack");
 
@@ -26,15 +28,17 @@ public class TestJackson {
         try {
             String json1 = mapper.writeValueAsString(user1);
             String json2 = mapper.writeValueAsString(user2);
-            System.out.println(json1);
+            System.out.println("user 1 as a json: " + json1);
 
+            //create a list with the objects as jsons
             List<String> jsons = List.of(json1, json2);
+            System.out.println(mapper.writeValueAsString("this is the list of jsons: " + jsons));
 
-            //convert jsons into a list of MyUser objects
+            //convert the list above into a list of MyUser objects
             List<MyUser> users = mapper.readValue(jsons.toString(), mapper.getTypeFactory().constructCollectionType(List.class, MyUser.class));
 
-            System.out.println(mapper.writeValueAsString(jsons));
-            System.out.println(mapper.writeValueAsString(users));
+
+            System.out.println(mapper.writeValueAsString("deserialized list: " + users));
         } catch (Exception e) {
             e.printStackTrace();
         }
